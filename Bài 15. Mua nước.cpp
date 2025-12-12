5c97189ff13cacc8ec958356043afa2f324979df
ducthinh2401 <ducthinh24.hn@gmail.com>
2025-12-12 11:11:18 +0700

    Update Bài 15. Mua nước.cpp

--- Bài 15. Mua nước.cpp
+++ Bài 15. Mua nước.cpp
@@ -1,3 +1,12 @@
+// 28tech muốn nấu một món súp. Để làm điều đó, anh ta cần mua chính xác n lít nước. Chỉ có hai loại chai nước trong cửa hàng gần đó -
+// chai 1 lít và chai 2 lít. Có vô số chai của hai loại này trong cửa hàng. Chai loại thứ nhất có gía a burles và chai loại thứ hai có giá tương ứng b burles. 
+// 28tech muốn chi càng ít tiền càng tốt.
+// Nhiệm vụ của bạn là tìm ra số tiền tối thiểu (bằng burles) 28tech cần mua chính xác n lít nước ở cửa hàng gần đó 
+// nếu chai loại thứ nhất có giá a burles và chai loại thứ hai có giá b burles
+
+
+
+
 #incude<iostream>
 #include<algorithm>
 
