206607f06771b2837f9c3cc4b0d204e9f67b7b10
ducthinh2401 <ducthinh24.hn@gmail.com>
2025-12-12 11:12:25 +0700

    Update Bài 16. Kí tự kế tiếp.cpp

--- Bài 16. Kí tự kế tiếp.cpp
+++ Bài 16. Kí tự kế tiếp.cpp
@@ -1,3 +1,8 @@
+// Cho kí tự là chữ cái in hoa hoặc in thường, in ra kí tự kế tiếp sau nó trong bảng chữ cái ở dạng in thường, 
+// tức là kí tự nhập vào ở dạng in hoa hay in thường thì bạn đều in ra kí tự kế tiếp nó nhưng ở dạng in thường. Coi kí tự kế tiếp của của chữ Z là chữ A.
+
+
+
 #include<iostream>
 
 using namespace std;
